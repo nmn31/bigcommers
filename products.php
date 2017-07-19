ad4339430f6ccb4845f9852267574ccfe096c25f
Narender <kumar.narender.dx@gmail.com>
2017-07-19 17:51:59 +0530

    s

--- products.php
+++ products.php
@@ -1,8 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Credentials: true");
-    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
-    header('Access-Control-Max-Age: 1000');
-    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-print_r([ 'status'=>'ok', 'data'=> ['1','2','3','4','5','6','7']]);
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
+print_r(json_encode([ 'status'=>'ok', 'data'=> ['1','2','3','4','5','6','7']]));
 ?>
