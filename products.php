c3332fc1db5c3ed4c0b0621ba842ac9bb598dfcf
Narender <kumar.narender.dx@gmail.com>
2017-07-19 17:23:33 +0530

    s

--- products.php
+++ products.php
@@ -1,14 +1,8 @@
 <?php
-$http_origin = $_SERVER['HTTP_ORIGIN'];
-
-$allowed_domains = array(
-  'https://store-hnsgk43w3q.mybigcommerce.com/',
-  'https://store-hnsgk43w3q.mybigcommerce.com/',
-);
-
-if (in_array($http_origin, $allowed_domains))
-{
-    header("Access-Control-Allow-Origin: $http_origin");
-}
+header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+    header('Access-Control-Max-Age: 1000');
+    header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 print_r(['1','2','3','4','5','6','7']);
 ?>
