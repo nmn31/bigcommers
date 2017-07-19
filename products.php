e144ea33e2f130686bee75a9aede46850a384d66
Narender <kumar.narender.dx@gmail.com>
2017-07-19 17:43:30 +0530

    s

--- products.php
+++ products.php
@@ -4,5 +4,5 @@ header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
     header('Access-Control-Max-Age: 1000');
     header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-print_r(['1','2','3','4','5','6','7']);
+print_r(["status"=>"ok" "data" => ['1','2','3','4','5','6','7']]);
 ?>
