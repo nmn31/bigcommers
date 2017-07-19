60e90cec36bd84bf2f38e3b8844ad9f66dfa39cc
Narender <kumar.narender.dx@gmail.com>
2017-07-19 17:08:09 +0530

    s

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-//header('Access-Control-Allow-Methods: GET, POST');
+header('Access-Control-Allow-Methods: GET, POST');
 header('content-type: application/json; charset=utf-8');
 print_r(['1','2','3','4','5','6','7']);
 ?>
