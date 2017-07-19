f608fc6afc0ca8c41f3aa4a33c5a91bd097afd07
Narender <kumar.narender.dx@gmail.com>
2017-07-19 17:13:43 +0530

    s

--- products.php
+++ products.php
@@ -1,6 +1,14 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header('Access-Control-Allow-Methods: GET, POST');
-header('content-type: application/json; charset=utf-8');
+$http_origin = $_SERVER['HTTP_ORIGIN'];
+
+$allowed_domains = array(
+  'https://store-hnsgk43w3q.mybigcommerce.com/',
+  'https://store-hnsgk43w3q.mybigcommerce.com/',
+);
+
+if (in_array($http_origin, $allowed_domains))
+{
+    header("Access-Control-Allow-Origin: $http_origin");
+}
 print_r(['1','2','3','4','5','6','7']);
 ?>
