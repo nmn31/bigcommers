36b02bf3276abdd51a08fff6218f6ae8980325ce
Narender <kumar.narender.dx@gmail.com>
2017-07-21 13:15:14 +0530

    dd

--- models.php
+++ models.php
@@ -9,17 +9,17 @@ if($_GET['id']==0){
   exit();
 };
 if($_GET['id']==5){
-  print_r(json_encode(['responce'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail']));
+  print_r(json_encode(['responce'=>'fail','test'=>'fail','test'=>'fail','testad'=>'fail','testzxcv'=>'failwe','test1'=>'fail','test32','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail']));
   exit();
 };
 if($_GET['id']==10){
-  print_r(json_encode(['responce'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail','test'=>'fail']));
+  print_r(json_encode(['responce'=>'fail','testq'=>'fail','testad'=>'fail','testzxcv'=>'failwe','test1'=>'fail','test32'=>'fail23','testw'=>'fail','test'=>'fail','test344'=>'fail','test444'=>'fail','test44444'=>'fail','test444444444'=>'fail','testw23'=>'fail','testrwr'=>'fail','testwre'=>'fail']));
   exit();
 };
-if($_GET['id']==10){
-  print_r(json_encode(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4' ]));
-  exit();
-};
+//if($_GET['id']==10){
+  //print_r(json_encode(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4' ]));
+//  exit();
+//};
 
-print_r(json_encode(['responce'=>'fail']));
+print_r(json_encode(['responce'=>'pass']));
 ?>
