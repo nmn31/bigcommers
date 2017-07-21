0add92bdc2bcd2dd212536d4a3a124dd7e5b7a76
Narender <kumar.narender.dx@gmail.com>
2017-07-21 11:55:46 +0530

    dd

--- models.php
+++ models.php
@@ -1,7 +1,12 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Credentials: true");
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 ECHO "RESULT";
 ECHO "BR";
 print_r($_GET);
 ECHO "BR";
-print_r(['1', '2', '3', '4', '5', '6', '7' ]);
+//print_r(['1', '2', '3', '4', '5', '6', '7' ]);
 ?>
