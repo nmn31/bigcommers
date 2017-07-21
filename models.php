0ca3fdf795aaf695d4dab505788427818b7f07e2
Narender <kumar.narender.dx@gmail.com>
2017-07-21 13:01:21 +0530

    dd

--- models.php
+++ models.php
@@ -5,19 +5,19 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 if($_GET['id']==0){
-  print_r(json_encode(['1', '2', '3', '4', '5' ]));
+  print_r(['1', '2', '3', '4', '5' ]);
   exit();
 };
 if($_GET['id']==5){
-  print_r(json_encode(array('1', '2', '3', '4', '5','1', '2', '3', '4', '5')));
+  print_r(json_encode(['1', '2', '3', '4', '5','1', '2', '3', '4', '5' ]));
   exit();
 };
 if($_GET['id']==10){
-  print_r(json_encode(array('1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5' ])));
+  print_r(json_encode(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5' ]));
   exit();
 };
 if($_GET['id']==10){
-  print_r(json_encode(array('1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4')));
+  print_r(json_encode(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4' ]));
   exit();
 };
 
