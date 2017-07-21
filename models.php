ec1b1919525d5c2984fa4db2cfe1778159cee786
Narender <kumar.narender.dx@gmail.com>
2017-07-21 12:10:38 +0530

    dd

--- models.php
+++ models.php
@@ -4,19 +4,19 @@ header("Access-Control-Allow-Credentials: true");
 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
-if($_GET['id']==0)){
+if($_GET['id']==0){
   print_r(['1', '2', '3', '4', '5' ]);
   exit();
 };
-if($_GET['id']==5)){
+if($_GET['id']==5){
   print_r(['1', '2', '3', '4', '5','1', '2', '3', '4', '5' ]);
   exit();
 };
-if($_GET['id']==10)){
+if($_GET['id']==10){
   print_r(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5' ]);
   exit();
 };
-if($_GET['id']==10)){
+if($_GET['id']==10){
   print_r(['1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4', '5','1', '2', '3', '4' ]);
   exit();
 };
