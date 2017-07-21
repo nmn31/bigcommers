d788d3f746923a757850f470abeca5e0f5e6d6b3
Narender <kumar.narender.dx@gmail.com>
2017-07-21 12:32:16 +0530

    dd

--- models.php
+++ models.php
@@ -5,7 +5,7 @@ header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
 header('Access-Control-Max-Age: 1000');
 header('Access-Control-Allow-Headers: Content-Type, Content-Range, Content-Disposition, Content-Description');
 if($_GET['id']==0){
-  print_r(['1', '2', '3', '4', '5' ]);
+  print_r(json_encode(['1', '2', '3', '4', '5' ]));
   exit();
 };
 if($_GET['id']==5){
@@ -21,5 +21,5 @@ if($_GET['id']==10){
   exit();
 };
 
-print_r(json_encode(['responce'='fail']));
+print_r(json_encode(['responce'=>'fail']));
 ?>
