3077df00e6bddb1ca58d7ef405aeaeeb0c5a15cf
Narender <kumar.narender.dx@gmail.com>
2017-08-23 12:51:11 +0530

    test

---
+++ api.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+ob_start();
+$json = file_get_contents('php://input');
+$request = json_decode($json, true);
+$action = $request["result"]["action"];
+$parameters = $request["result"]["parameters"];
+
+[Code to set $outputtext, $nextcontext, $param1, $param2 values]
+
+$output["contextOut"] = array(array("name" => "$next-context", "parameters" =>
+array("param1" => $param1value, "param2" => $param2value)));
+$output["speech"] = $outputtext;
+$output["displayText"] = $outputtext;
+$output["source"] = "whatever.php";
+ob_end_clean();
+echo json_encode($output);
+?>
